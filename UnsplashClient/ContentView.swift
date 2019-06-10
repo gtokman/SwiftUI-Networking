257f1a400d0ea93df24c1e3e9bb6da7c6c03e379
Gary Tokman <hello@garytokman.me>
2019-06-09 21:12:41 -0400

    Initial Commit

---
+++ UnsplashClient/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UnsplashClient
+//
+//  Created by Gary Tokman on 6/9/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
