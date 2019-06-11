73365271f26668471c6aaffb73ba2522d70addee
Gary Tokman <hello@garytokman.me>
2019-06-10 20:27:07 -0400

    initial setup

---
+++ UnsplashClient/LoadingView.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingView.swift
+//  UnsplashClient
+//
+//  Created by Gary Tokman on 6/9/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct LoadingView_Previews : PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
+#endif
---
+++ UnsplashClient/Unsplash.swift
@@ -0,0 +1,9 @@
+//
+//  Unsplash.swift
+//  UnsplashClient
+//
+//  Created by Gary Tokman on 6/9/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import Foundation
